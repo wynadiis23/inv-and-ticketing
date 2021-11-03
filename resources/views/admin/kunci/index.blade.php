88ba26db78d3bcb2c42c1a70d1a07496fb8fad59
Adi Saputra <adii.empty@gmail.com>
2021-11-03 17:46:54 +0800

    datatable di kunci index dan fix search

--- resources/views/admin/kunci/index.blade.php
+++ resources/views/admin/kunci/index.blade.php
@@ -8,20 +8,25 @@
     <div class="card-body">
         <div class="table-responsive">
         <!-- FORM UNTUK FILTER BERDASARKAN DATE RANGE -->
-            <form action="{{ route('admin.peminjaman.rangeReport') }}" method="get">
-                <div class="input-group mb-3 col-md-3 float-right">
-                    <input type="text" id="created_at" name="date" class="form-control">
-                    <div class="input-group-append">
-                        <button class="btn btn-secondary" type="submit">Filter</button>
+            <div class="row input-daterange">
+                <div class="input-group col-md-4">
+                    <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" readonly />
+                </div>
+                <div class="input-group col-md-4">
+                    <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date" readonly />
+                </div>
+                <div class="input-group col-md-4">
+                    <div class="col-md-2">
+                        <button type="button" name="filter" id="filter" class="btn btn-primary">Filter</button>
+                    </div>
+                    <div class="col-md-2">
+                        <button type="button" name="refresh" id="refresh" class="btn btn-default">Refresh</button>
                     </div>
                 </div>
-            </form>
-            <table class=" table table-bordered table-striped table-hover datatable datatable-peminjaman">
+            </div>
+            <table class=" table table-bordered table-striped table-hover datatable datatable-kunci">
                 <thead>
                     <tr>
-                        <th width="10">
-
-                        </th>
                         <th>
                             ID
                         </th>
@@ -32,46 +37,11 @@
                             Peminjam
                         </th>
                         <th>
-                            Tanggal Peminjaman
-                        </th>
-                        <th>
-                            Status
-                        </th>
-                        <th>
-                            Admin
+                            Tanggal Dibuat
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($kuncis as $key => $kunci)
-                        <tr data-entry-id="{{ $kunci->id }}">
-                            <td>
-
-                            </td>
-                            <td>
-                                {{ $kunci->id ?? '' }}
-                            </td>
-                            <td>
-                                {{ $kunci->kunci ?? '' }}
-                            </td>
-                            <td>
-                                {{ $kunci->peminjaman->nama ?? '' }}
-                            </td>
-                            <td>
-                                {{ $kunci->peminjaman->tanggal_pinjam ?? '' }}
-                            </td>
-                            <td>
-                            @if ($kunci->status === 1)
-                                Sudah Terpakai
-                            @else
-                                Belum Terpakai
-                            @endif
-                            </td>
-                            <td>
-                                {{ $kunci->peminjaman->user_id ?? '' }}
-                            </td>
-                        </tr>
-                    @endforeach
                 </tbody>
             </table>
         </div>
@@ -82,63 +52,100 @@
 @endsection
 @section('scripts')
 @parent
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js"></script>
+<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script> -->
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
 <script>
     $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('peminjaman_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.peminjaman.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
+        $('.input-daterange').datepicker({
+            todayBtn:'linked',
+            format:'yyyy-mm-dd',
+            autoclose:true
+        });
 
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
+        load_data();
 
-        return
-      }
+        function load_data(from_date = '', to_date = ''){
+            alert(from_date);
+                    //datatable ajax
+            var table = $('.datatable-kunci').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: '{{ route('admin.kunci.index') }}',
+                    data: {from_date:from_date, to_date:to_date},
+                },
+                columns: [
+                    {data: 'id', name: 'id'},
+                    {data: 'kunci', name: 'kunci'},
+                    {data: 'nama', name: 'nama'},
+                    {data: 'created_at', name: 'created_at'},
+                ]
+            });
+        }
 
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
+        $('#filter').click(function(){
+            var from_date = $('#from_date').val();
+            var to_date = $('#to_date').val();
+            if(from_date != '' &&  to_date != '')
+            {
+                $('.datatable-kunci').DataTable().destroy();
+                load_data(from_date, to_date);
+            }
+            else
+            {
+                alert('Both Date is required');
+            }
+        });
+
+        $('#refresh').click(function(){
+            $('#from_date').val('');
+            $('#to_date').val('');
+            $('.datatable-kunci').DataTable().destroy();
+            load_data();
+        });
+//   let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+// @can('peminjaman_delete')
+//   let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
+//   let deleteButton = {
+//     text: deleteButtonTrans,
+//     url: "{{ route('admin.peminjaman.massDestroy') }}",
+//     className: 'btn-danger',
+//     action: function (e, dt, node, config) {
+//       var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
+//           return $(entry).data('entry-id')
+//       });
+
+//       if (ids.length === 0) {
+//         alert('{{ trans('global.datatables.zero_selected') }}')
+
+//         return
+//       }
+
+//       if (confirm('{{ trans('global.areYouSure') }}')) {
+//         $.ajax({
+//           headers: {'x-csrf-token': _token},
+//           method: 'POST',
+//           url: config.url,
+//           data: { ids: ids, _method: 'DELETE' }})
+//           .done(function () { location.reload() })
+//       }
+//     }
   }
   dtButtons.push(deleteButton)
 @endcan
 
-  $.extend(true, $.fn.dataTable.defaults, {
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  $('.datatable-peminjaman:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
-        $($.fn.dataTable.tables(true)).DataTable()
-            .columns.adjust();
-    });
-})
-$(document).ready(function() {
-    let start = moment().startOf('month')
-    let end = moment().endOf('month')
-
-    //INISIASI DATERANGEPICKER
-    $('#created_at').daterangepicker({
-        startDate: start,
-        endDate: end
-    })
+//   $.extend(true, $.fn.dataTable.defaults, {
+//     order: [[ 1, 'desc' ]],
+//     pageLength: 100,
+//   });
+//   $('.datatable-peminjaman:not(.ajaxTable)').DataTable({ buttons: dtButtons })
+//     $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+//         $($.fn.dataTable.tables(true)).DataTable()
+//             .columns.adjust();
+//     });
 })
 
 </script>
